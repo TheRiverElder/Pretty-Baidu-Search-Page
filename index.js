5711bef96292b5e54ced339171882bd4caafd2e9
TheRiverElder <765375399@qq.com>
2020-06-22 21:30:22 +0800

    disabled on baidu main page (enabled in search result page)

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 // @name         百度搜索页面双列美化
 // @name:en      Pretty Baidu Search Page
 // @namespace    https://github.com/TheRiverElder/Pretty-Baidu-Search-Page/blob/master/index.js
-// @version      1.0.2
+// @version      1.0.3
 // @description  美化百度搜索页面，去除广告、相关关键词、提供自定义的图片背景、毛玻璃圆角卡片、双列布局。双列布局采用紧密布局，不会出现某个搜索结果有过多空白。
 // @description:en  Prettify Baidu search page. Removed the ads, relative keywords. Offers custom image or color backgroud. Uses round corner card to display result. Densitive layout ensures no more blank in result cards.
 // @author       TheRiverElder
 // @icon         https://theriverelder.github.io/assets/river_icon_dark.ico
 // @compatible   chrome
-// @include      *//www.baidu.com/*
+// @include      *//www.baidu.com/s?*
 // @grant        GM_addStyle
 // @grant        GM_getValue
 // @grant        GM_setValue
